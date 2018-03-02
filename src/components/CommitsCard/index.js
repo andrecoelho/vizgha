46997f8b434b34fa377cf70bb58bbca048d7389a
Andre Coelho <acoelho@riskalyze.com>
2018-03-01 23:22:00 -0800

    Add page title and simple list of commits

--- src/components/CommitsCard/index.js
+++ src/components/CommitsCard/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { Card, CardTitle } from 'material-ui/Card'
 import Divider from 'material-ui/Divider'
 import IconButton from 'material-ui/IconButton'
+import SvgBack from 'material-ui/svg-icons/image/navigate-before'
 import SvgToken from 'material-ui/svg-icons/communication/vpn-key'
 
 import styles from './commits-card.scss'
@@ -12,11 +13,18 @@ class CommitsCard extends Component {
   constructor (props) {
     super(props)
 
-    this.state = {
-      commits: []
-    }
-
+    this.showRepositories = this.showRepositories.bind(this)
     this.changeToken = this.changeToken.bind(this)
+  }
+
+  componentWillMount () {
+    if (this.props.commits.length === 0) {
+      this.props.apiCommits(this.props.match.params.name)
+    }
+  }
+
+  showRepositories () {
+    this.props.history.push(`/repos${this.props.location.search}`)
   }
 
   changeToken () {
@@ -28,7 +36,15 @@ class CommitsCard extends Component {
       <Card className={styles.main}>
         <div styleName='content'>
           <div styleName='title'>
-            <CardTitle title='Commits' />
+            <IconButton tooltip='Repositories' onClick={this.showRepositories}>
+              <SvgBack />
+            </IconButton>
+
+            <CardTitle
+              title='Commits'
+              subtitle={this.props.match.params.name}
+              className={styles.titleText}
+            />
 
             <IconButton tooltip='Change Token' onClick={this.changeToken}>
               <SvgToken />
@@ -36,6 +52,12 @@ class CommitsCard extends Component {
           </div>
 
           <Divider />
+
+          <div>
+            {this.props.commits.map(commit => (
+              <div key={commit.oid}>{commit.message}</div>
+            ))}
+          </div>
         </div>
       </Card>
     )
@@ -43,8 +65,11 @@ class CommitsCard extends Component {
 }
 
 CommitsCard.propTypes = {
+  apiCommits: PropTypes.func.isRequired,
+  commits: PropTypes.array.isRequired,
   history: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired
+  location: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired
 }
 
 export default CommitsCard
--- src/views/Commits/index.js
+++ src/views/Commits/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import _ from 'lodash/fp'
 
 import CommitsCard from 'components/CommitsCard'
-
+import { apiCommits } from 'store/actions/creators'
 import './commits.scss'
 
 const Commits = props => (
@@ -11,8 +12,21 @@ const Commits = props => (
   </div>
 )
 
-const mapStateToProps = state => ({
-  commits: state.commits
+const mapStateToProps = (state, ownProps) => {
+  const repo = _.find(
+    repo => repo.name === ownProps.match.params.name,
+    state.repos
+  )
+
+  return {
+    commits: _.propOr([], 'commits', repo)
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  apiCommits (userName, repoName) {
+    dispatch(apiCommits(userName, repoName))
+  }
 })
 
-export default connect(mapStateToProps)(Commits)
+export default connect(mapStateToProps, mapDispatchToProps)(Commits)
